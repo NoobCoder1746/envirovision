d311742ee922736710989af65cfbe26de28510c1
NoobCoder1746 <122960691+NoobCoder1746@users.noreply.github.com>
2025-10-20 02:28:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.title("♻️ EnviroVision - AI phân loại rác")
+st.title("♻️ EnviroVision - Demo AI phân loại rác by Hải và Phát 11A5")
 uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
 conf_threshold = 0.0
